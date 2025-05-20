db268228de572fe5b5b15e875207c2e498573dde
Ömer Faruk Uzkal <147771249+ofuzkal@users.noreply.github.com>
2025-05-20 17:17:14 +0300

    react  component inline css kullanımı

--- src/ProductItem.jsx
+++ src/ProductItem.jsx
@@ -1,10 +1,21 @@
+const productItemCSS = {
+  border: "1px solid red",
+  backgroundColor: "black",
+  color: "white",
+};
+
+const productImageCSS = {
+  width: "100px",
+};
+
 function ProductItem() {
   return (
-    <div className="product-item">
+    <div className="product-item" style={productItemCSS}>
       <div className="product-image">
         <img
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREaXGeoatJyRmp9Aa-q4zazgcUQiM3bQHvjg&usqp=CAU"
           alt="Soda"
+          style={productImageCSS}
         />
       </div>
       <div className="product-info">
