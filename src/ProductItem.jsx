4f73beb67e6ee34194ddcb310b7577be5de7b62f
Ömer Faruk Uzkal <147771249+ofuzkal@users.noreply.github.com>
2025-05-21 14:59:01 +0300

    props kullanımı

--- src/ProductItem.jsx
+++ src/ProductItem.jsx
@@ -1,19 +1,22 @@
 import "./ProductItem.css";
 
-function ProductItem() {
-  const image ="https://ardenmarket.com.tr/media/catalog/product/cache/ce320e98947e5c83f08a8e256dc8423e/b/e/beypazari-soda-200ml.jpg"
-  const title="Soda";
-  const price=10
+function ProductItem(props) {
+  console.log(props);
+  console.log(props.image);
+  console.log(props.title);
+  console.log(props.price);
+
+  
   
   return (
     <div className="product-item">
       <div className="product-image">
-        <img src={image} alt="Soda" />
+        <img src={props.image} alt="Soda" />
           
       </div>
       <div className="product-info">
-        <b className="product-title">{title}</b>
-        <span className="product-price">{price}</span>
+        <b className="product-title">{props.title}</b>
+        <span className="product-price">{props.price}</span>
       </div>
     </div>
   );
--- src/Products.jsx
+++ src/Products.jsx
@@ -6,11 +6,18 @@ function Products() {
     <div className="products">
       <h1>Produc Component </h1>
       <div className="products-wrapper">
-        <ProductItem />
-        <ProductItem />
-        <ProductItem />
-        <ProductItem />
-        <ProductItem />
+        <ProductItem
+          image={
+            "https://ardenmarket.com.tr/media/catalog/product/cache/ce320e98947e5c83f08a8e256dc8423e/b/e/beypazari-soda-200ml.jpg"
+          }
+          title="Soda"
+          price={10}
+        />
+        <ProductItem
+          image="https://st-vans.mncdn.com/mnresize/1500/1500/Content/media/ProductImg/original/638574215162138731.jpg"
+          title="Tisört"
+          price={10}
+        />
       </div>
     </div>
   );
