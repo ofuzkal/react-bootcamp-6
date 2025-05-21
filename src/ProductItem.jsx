dd526dc43517e5dc587e61d79c4b8be35d2ab58d
Ömer Faruk Uzkal <147771249+ofuzkal@users.noreply.github.com>
2025-05-21 12:26:51 +0300

    react conponentlerinde javascript değişkenlikleri tanımla

--- src/ProductItem.jsx
+++ src/ProductItem.jsx
@@ -1,17 +1,19 @@
 import "./ProductItem.css";
 
 function ProductItem() {
+  const image ="https://ardenmarket.com.tr/media/catalog/product/cache/ce320e98947e5c83f08a8e256dc8423e/b/e/beypazari-soda-200ml.jpg"
+  const title="Soda";
+  const price=10
+  
   return (
     <div className="product-item">
       <div className="product-image">
-        <img
-          src="https://ardenmarket.com.tr/media/catalog/product/cache/ce320e98947e5c83f08a8e256dc8423e/b/e/beypazari-soda-200ml.jpg"
-          alt="Soda"
-        />
+        <img src={image} alt="Soda" />
+          
       </div>
       <div className="product-info">
-        <b className="product-title">Soda</b>
-        <span className="product-price">4₺</span>
+        <b className="product-title">{title}</b>
+        <span className="product-price">{price}</span>
       </div>
     </div>
   );
