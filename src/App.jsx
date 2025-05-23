e5bb8f76ac7ce73bad15cc3f1a7978cd879aad5d
Ömer Faruk Uzkal <147771249+ofuzkal@users.noreply.github.com>
2025-05-23 11:05:17 +0300

    lokal sstate tanımlama

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
 import Products from "./Products";
 
 function App() {
+  const [state, setState] = useState("Ömer");
+
+ console.log("component render edildi");
+  // state = "Emre" // yanlış kullanım
+  // setState("Emre") // doğru kullanım
+
+  function handleTitleChange() {
+    setState("Emin");
+  }
+
   return (
     <div>
-     <Products />
+      <h2>{state}</h2>
+      <button onClick={handleTitleChange}>Değiştir!</button>
     </div>
   );
 }
