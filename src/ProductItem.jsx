a956b4856954a9717b6d91980dc3dfe7ab545a11
Ömer Faruk Uzkal <147771249+ofuzkal@users.noreply.github.com>
2025-05-23 12:45:32 +0300

    local title state

--- src/ProductItem.jsx
+++ src/ProductItem.jsx
@@ -1,12 +1,14 @@
+import { useState } from "react";
 import "./ProductItem.css";
 
 function ProductItem(props) {
   const { image, globalTitle, setGlobalTitle, title, price } = props;
+  const [localTitleState, setTitleLocalState] = useState(title);
 
   console.log(props);
 
   function handleTitleChange() {
-    setGlobalTitle("Yüzük");
+    setTitleLocalState("Yüzük");
   }
 
   console.log("component render edildi!");
@@ -17,9 +19,7 @@ function ProductItem(props) {
         <img src={image} alt="Soda" />
       </div>
       <div className="product-info">
-        <b className="product-title">
-          {globalTitle !== "" ? globalTitle : title}
-        </b>
+        <b className="product-title">{localTitleState}</b>
         <span className="product-price">{price}₺</span>
         <button onClick={handleTitleChange}>Title Değiştir!</button>
       </div>
